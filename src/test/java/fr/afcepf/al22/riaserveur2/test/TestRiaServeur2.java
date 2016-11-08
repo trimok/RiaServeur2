6626b9f90c4c992b3effd867fd9b39aab5fd2f54
trimok <trimok@mail.fr>
2016-11-08 14:07:59 +0100

    Assert dans testriaserveur2

--- src/test/java/fr/afcepf/al22/riaserveur2/test/TestRiaServeur2.java
+++ src/test/java/fr/afcepf/al22/riaserveur2/test/TestRiaServeur2.java
@@ -1,11 +1,13 @@
 package fr.afcepf.al22.riaserveur2.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestRiaServeur2 {
 
 	@Test
 	public void test() {
+		Assert.assertTrue(true);
 		// fail("Not yet implemented");
 	}
 
